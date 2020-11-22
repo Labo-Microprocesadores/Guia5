51599f05c262f14afd8d7b418d71b7946a16a9a0
scheinpablo <scheinpablo@yahoo.com>
2020-11-22 18:15:41 -0300

    refactor spi guia

--- SPI_drv/drivers/spiguia.h
+++ SPI_drv/drivers/spiguia.h
@@ -4,65 +4,105 @@
 #include "stdint.h"
 #include "stdbool.h"
 
+typedef enum
+{
+	SPI_CTAR_0,
+	SPI_CTAR_1
+} SPI_CTAR;
 
+typedef enum
+{
+	SPI_PCS_0,
+	SPI_PCS_1,
+	SPI_PCS_2,
+	SPI_PCS_3,
+	SPI_PCS_4,
+	SPI_PCS_5
+} SPI_PCSignal;
 
+typedef enum
+{
+	SPI_fourBitsFrame = 3,
+	SPI_fiveBitsFrame,
+	SPI_sixBitsFrame,
+	SPI_sevenBitsFrame,
+	SPI_eightBitsFrame,
+	SPI_nineBitsFrame,
+	SPI_tenBitsFrame,
+	SPI_elevenBitsFrame,
+	SPI_twelveBitsFrame,
+	SPI_thirteenBitsFrame,
+	SPI_fourteenBitsFrame,
+	SPI_fifteenBitsFrame,
+	SPI_sixteenBitsFrame
+} SPI_BitsPerFrame;
 
+typedef enum
+{
+	SPI_onePowerDelay,
+	SPI_twoPowerDelay,
+	SPI_threePowerDelay,
+	SPI_fourPowerDelay,
+	SPI_fivePowerDelay,
+	SPI_sixPowerDelay,
+	SPI_sevenPowerDelay,
+	SPI_eightPowerDelay,
+	SPI_ninePowerDelay,
+	SPI_tenPowerDelay
+} SPI_CLKDelayScaler;
 
-typedef enum {SPI_CTAR_0, SPI_CTAR_1}SPI_CTAR;
-
-typedef enum {SPI_PCS_0, SPI_PCS_1, SPI_PCS_2, SPI_PCS_3, SPI_PCS_4, SPI_PCS_5}SPI_PCSignal;
-
-typedef enum {
-	SPI_fourBitsFrame = 3, SPI_fiveBitsFrame, SPI_sixBitsFrame, SPI_sevenBitsFrame, SPI_eightBitsFrame,
-	SPI_nineBitsFrame, SPI_tenBitsFrame, SPI_elevenBitsFrame, SPI_twelveBitsFrame, SPI_thirteenBitsFrame,
-	SPI_fourteenBitsFrame, SPI_fifteenBitsFrame, SPI_sixteenBitsFrame
-}SPI_BitsPerFrame;
-
-typedef enum {
-	SPI_onePowerDelay, SPI_twoPowerDelay, SPI_threePowerDelay, SPI_fourPowerDelay, SPI_fivePowerDelay,
-	SPI_sixPowerDelay, SPI_sevenPowerDelay, SPI_eightPowerDelay, SPI_ninePowerDelay, SPI_tenPowerDelay
-}SPI_CLKDelayScaler;
-
-typedef enum {
+typedef enum
+{
 	SPI_DelayAfterTransferPreScaleOne,
 	SPI_DelayAfterTransferPreScaleThree,
 	SPI_DelayAfterTransferPreScaleFive,
 	SPI_DelayAfterTransferPreScaleSeven
-}SPI_DelayAfterTransferPreScale;
+} SPI_DelayAfterTransferPreScale;
 
-typedef enum {
+typedef enum
+{
 	SPI_PCSActiveHigh,
 	SPI_PCSActiveLow,
-}SPI_ChipSelectActiveState;
+} SPI_ChipSelectActiveState;
 
-typedef enum {
+typedef enum
+{
 	SPI_ClockActiveHigh,
 	SPI_ClockActiveLow,
-}SPI_ClockPolarity;
+} SPI_ClockPolarity;
 
-typedef enum{
+typedef enum
+{
 	SPI_ClockPhaseFirstEdge,
 	SPI_ClockPhaseSecondEdge,
-}SPI_ClockPhase;
+} SPI_ClockPhase;
 
-typedef enum{
+typedef enum
+{
 	SPI_FirstMSB,
 	SPI_FirstLSB,
-}SPI_ShiftDirection;
+} SPI_ShiftDirection;
 
-typedef enum{
+typedef enum
+{
 	SPI_SlaveSelectAsGPIO,
 	SPI_SlaveSelectFaultPin,
 	SPI_SlaveSelectAutomaticOutput,
-}SPI_OutputMode;
+} SPI_OutputMode;
 
-typedef enum{
+typedef enum
+{
 	SPI_PinModeNormal,
 	SPI_PinModeInput,
 	SPI_PinModeOutput,
-}SPI_PinMode;
+} SPI_PinMode;
 
-typedef enum {SPI_0, SPI_1, SPI_2}SPI_Instance;
+typedef enum
+{
+	SPI_0,
+	SPI_1,
+	SPI_2
+} SPI_Instance;
 
 typedef struct
 {
@@ -79,46 +119,13 @@ typedef struct
 	SPI_OutputMode outputMode;
 	SPI_PinMode pinMode;
 	uint32_t baudRate;
-}SPI_MasterConfig;
+} SPI_MasterConfig;
 
-
-void SPI_MasterInit(SPI_Instance n, SPI_MasterConfig * config);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+void SPI_MasterInit(SPI_Instance n, SPI_MasterConfig *config);
 
 typedef void (*SPI_Callback)(void);
 
-void SPI_MasterGetDefaultConfig(SPI_MasterConfig * config);
-
+void SPI_MasterGetDefaultConfig(SPI_MasterConfig *config);
 
 void SPI_EnableTxFIFOFillDMARequests(SPI_Instance n);
 
@@ -132,8 +139,8 @@ void SPI_DisableEOQInterruptRequests(SPI_Instance n);
 
 uint32_t SPI_GetDataRegisterAddress(SPI_Instance n);
 
-bool SPI_SendByte( uint8_t byte);
+bool SPI_SendByte(uint8_t byte);
 
-int SPI_SendFrame(uint8_t * data, uint8_t length, SPI_Callback callback);
+int SPI_SendFrame(uint8_t *data, uint8_t length, SPI_Callback callback);
 
 #endif /* SPI_H_ */
