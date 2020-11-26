32d013062a965714d459700b67d8236225c9c76c
scheinpablo <scheinpablo@yahoo.com>
2020-11-25 22:04:29 -0300

    sensors position angle data struct

--- i2c2/i2c+main/projects/Blink/source/SensorsPosition.c
+++ i2c2/i2c+main/projects/Blink/source/SensorsPosition.c
@@ -11,32 +11,27 @@
 #include "AccelMagn_drv.h"
 #include "timer.h"
 #include <math.h>
-
 /*******************************************************************************
  * ENUMERATIONS AND STRUCTURES AND TYPEDEFS
  ******************************************************************************/
-enum{CLOCKWISE, COUNTER_CLOCKWISE, ZERO};
-
+typedef struct
+{
+	int16_t currentValue, previousValue;
+	bool valueChanged;
+}SensorsPosition_AngleData_t;
 
 /*******************************************************************************
  * VARIABLE PROTOTYPES WITH GLOBAL SCOPE
  ******************************************************************************/
 SRAWDATA accelerometerCoordinates = {.x = -1, .y = -1, .z = -1};
-SRAWDATA magentometerCoordinates = {.x = 0, .y = -1, .z = 0}; //
+SRAWDATA magnetometerCoordinates = {.x = 0, .y = -1, .z = 0}; //
 
-int16_t roll = 0; 		// Form -179° to 180°
-int16_t previousRoll = 0;
-bool rollChanged = false;
+SensorsPosition_AngleData_t roll ={.currentValue = 0, .previousValue = 0, .valueChanged = false};
+SensorsPosition_AngleData_t pitch ={.currentValue = 0, .previousValue = 0, .valueChanged = false};
+SensorsPosition_AngleData_t yaw ={.currentValue = 0, .previousValue = 0, .valueChanged = false};
 
-int16_t pitch = 0;	// Form -179° to 180°
-int16_t previousPitch = 0;
-bool pitchChanged = false;
 
-int16_t yaw = 0;	// Form -179° to 180°
-int16_t previousYaw = 0;
-bool yawChanged = false;
-
-static read_data accelerometerData;
+static read_data sensorsData;
 static bool isReading = false;
 
 void (*__onAngleChangedCallback)(void);
@@ -47,7 +42,7 @@ void (*__onAngleChangedCallback)(void);
  ******************************************************************************/
 
 static void Position_CalculateRoll(void);
-static void Position_CalculatePitching(void);
+static void Position_CalculatePitch(void);
 static void Position_CalculateYaw(void);
 static void callback_updatePos (void);
 
@@ -59,14 +54,14 @@ static void callback_updatePos (void);
 void SensorsPosition_Init(void (*onAngleChangedCallback)(void)){
 	AccelMagn_init();
 	_onAngleChangedCallback = onAngleChangedCallback;
-	accelerometerData.callback = callback_updatePos;
-	accelerometerData.pAccelData = &accelerometerCoordinates;
-	accelerometerData.pMagnData = &magentometerCoordinates;
+	sensorsData.callback = callback_updatePos;
+	sensorsData.pAccelData = &accelerometerCoordinates;
+	sensorsData.pMagnData = &magnetometerCoordinates;
 }
 
 static void callback_updatePos (void)
 {
-	if (!accelerometerData.error == I2C_OK)
+	if (!sensorsData.error == I2C_OK)
 	{
 		isReading = false;
 		return;
@@ -80,219 +75,94 @@ void SensorsPosition_ReadAccelerometer(void)
 {
 	if (isReading)
 		return;
-	AccelMagn_getData(&accelerometerData);
+	AccelMagn_getData(&sensorsData);
 
 }
 
 int16_t SensorsPosition_GetRollAngle(void)
 {
-	return roll;
+	return roll.currentValue;
 }
 
 int16_t SensorsPosition_GetPitchAngle(void)
 {
-	return pitch;
+	return pitch.currentValue;
 }
 
 int16_t SensorsPosition_GetYawAngle(void)
 {
-	return yaw;
+	return yaw.currentValue;
 }
 
 SensorsPosition_Angles_t SensorsPosition_GetChangedAngle(void)
 {
-	if(rollChanged)
+	if(roll.valueChanged)
 	{
-		rollChanged = false;
+		roll.valueChanged = false;
 		return ROLL;
 	}
-	else if(pitchChanged)
+	else if(pitch.valueChanged)
 	{
-		pitchChanged = false;
+		pitch.valueChanged = false;
 		return PITCH;
 	}
-	else if(yawChanged)
+	else if(yaw.valueChanged)
 	{
-		yawChanged = false;
+		yaw.valueChanged = false;
 		return YAW;
 	}
 }
 
 static void Position_CalculateRoll(void)
 {
-	uint8_t clock_type;
-	int theta;
-
+	roll.previousValue = roll.currentValue;
 	if (accelerometerCoordinates.x != 0)
-		theta = (int)180*atan((float)accelerometerCoordinates.z/ (float)accelerometerCoordinates.x)/M_PI;
+		roll.currentValue = (int)180*atan((float)accelerometerCoordinates.z/ (float)accelerometerCoordinates.x)/M_PI;
 	else
-		theta = 90;
+		roll.currentValue = 90;
 
 
-	/*if(accelerometerCoordinates.x < 0){
-		clock_type = CLOCKWISE;
-	}else if(accelerometerCoordinates.x > 0){
-		clock_type = COUNTER_CLOCKWISE;
-	}else{
-		clock_type = ZERO;
-	}
-
-	if(clock_type != ZERO){
-
-		theta = (int)180*atan((float)accelerometerCoordinates.z/ (float)accelerometerCoordinates.x)/M_PI;
-
-	}else{
-		theta = 90;
-	}*/
-
-	switch (clock_type) {
-		case CLOCKWISE:
-			if(accelerometerCoordinates.z != 0){
-				previousRoll = - 90 - theta;
-			}else{
-				previousRoll = - 90;
-			}
-			break;
-		case COUNTER_CLOCKWISE:
-			if(accelerometerCoordinates.z != 0){
-				previousRoll = 90 - theta;
-			}else{
-				previousRoll = 90;
-			}
-			break;
-
-		case ZERO:
-			if(accelerometerCoordinates.z > 0){
-				previousRoll = 0;
-			}else if(accelerometerCoordinates.z < 0){
-				previousRoll = 180;
-			}
-			break;
-	}
-
-	int diference = fabsf((fabsf(previousRoll) - fabsf(roll)));
-
-	if(diference > 5) // Condicion de consigna para tomar nuevo cambio
-	{
-		roll = previousRoll;
-		rollChanged = true;
-		_onAngleChangedCallback(); // Aviso a App que cambio el angulo mas de 5°
-	}
-}
-
-static void Position_CalculatePitching(void)
-{
-	uint8_t clock_type;
-	int theta;
-
-	if(accelerometerCoordinates.y < 0){
-		clock_type = CLOCKWISE;
-	}else if(accelerometerCoordinates.y > 0){
-		clock_type = COUNTER_CLOCKWISE;
-	}else{
-		clock_type = ZERO;
-	}
-
-	if(clock_type != ZERO){
-
-		theta = (int)180*atan((float)accelerometerCoordinates.z/ (float)accelerometerCoordinates.y)/M_PI;
-
-	}else{
-		theta = 90;
-	}
-
-	switch (clock_type) {
-		case CLOCKWISE:
-			if(accelerometerCoordinates.z != 0){
-				previousPitch = - 90 - theta;
-			}else{
-				previousPitch = - 90;
-			}
-			break;
-		case COUNTER_CLOCKWISE:
-			if(accelerometerCoordinates.z != 0){
-				previousPitch = 90 - theta;
-			}else{
-				previousPitch = 90;
-			}
-			break;
-		case ZERO:
-			if(accelerometerCoordinates.z < 0){
-				previousPitch = 0;
-			}else if(accelerometerCoordinates.z > 0){
-				previousPitch = 180; // Por defecto, podria ser tambien
-			}
-			break;
-	}
-
-	int diference = fabsf(fabsf(previousPitch) - fabsf(pitch));
+	int diference = fabsf((fabsf(roll.previousValue) - fabsf(roll.currentValue)));
 
 	if(diference > 5)
 	{
-		pitch = previousPitch;
-		pitchChanged = true;
-		_onAngleChangedCallback(); // Aviso a App que cambio el angulo mas de 5°
+		roll.valueChanged = true;
+		_onAngleChangedCallback();
+	}
+}
+
+static void Position_CalculatePitch(void)
+{
+	pitch.previousValue = pitch.currentValue;
+	if (accelerometerCoordinates.y != 0)
+		pitch.currentValue = (int)180*atan((float)accelerometerCoordinates.z/ (float)accelerometerCoordinates.y)/M_PI;
+	else
+		pitch.currentValue = 90;
+
+	int diference = fabsf(fabsf(pitch.previousValue) - fabsf(pitch.currentValue));
+
+	if(diference > 5)
+	{
+		pitch.valueChanged = true;
+		_onAngleChangedCallback();
 	}
 }
 
 static void Position_CalculateYaw(void)
 {
-
-	uint8_t clock_type;
-	int theta;
-
-	if(magentometerCoordinates.y < 0){
-		clock_type = CLOCKWISE;
-	}else if(magentometerCoordinates.y > 0){
-		clock_type = COUNTER_CLOCKWISE;
-	}else{
-		clock_type = ZERO;
-	}
-
-	if(clock_type != ZERO){
-
-		theta = (int)180*atan((float)magentometerCoordinates.y/ (float)magentometerCoordinates.x)/M_PI;
-
-	}else{
-		theta = 90;
-	}
-
-	switch (clock_type) {
-		case CLOCKWISE:
-			if(magentometerCoordinates.x > 0){
-				previousYaw = - 90 + theta;
-			}else if(magentometerCoordinates.x < 0){
-				previousYaw = 90 + theta;
-			}else{
-				previousYaw = 180;
-			}
-			break;
-		case COUNTER_CLOCKWISE:
-			if(magentometerCoordinates.x < 0){
-				previousYaw = 90 + theta;
-			}else if(magentometerCoordinates.x > 0){
-				previousYaw = - 90 + theta;
-			}else{
-				previousYaw = 0;
-			}
-			break;
-		case ZERO:
-			if(magentometerCoordinates.x < 0){
-				previousYaw = theta;
-			}else if(magentometerCoordinates.x > 0){
-				previousYaw = 0 - theta; // Por defecto, podria ser tambien
-			}
-			break;
-	}
+	yaw.previousValue = yaw.currentValue;
+	if (magnetometerCoordinates.y != 0)
+		yaw.currentValue =  (int)180*atan((float)magnetometerCoordinates.y/ (float)magnetometerCoordinates.x)/M_PI;
+	else
+		yaw.currentValue = 90;
 
 
-	int diference = fabsf(fabsf(previousYaw) - fabsf(yaw));
+	int diference = fabsf(fabsf(yaw.previousValue) - fabsf(yaw.currentValue));
 
 	if(diference > 5)
 	{
-		yaw = previousYaw;
-		yawChanged = true;
-		_onAngleChangedCallback(); // Aviso a App que cambio el angulo mas de 5°
+		yaw.valueChanged = true;
+		_onAngleChangedCallback();
 	}
 }
 
